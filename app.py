1314171311951045c47e59228c53e7bb991d79d1
nicolasdosantos <104533715+nicolasdosantos@users.noreply.github.com>
2023-12-15 07:57:06 -0300

    --------------

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
-from flask import Flask
-
+from flask import Flask, render_template,request,redirect
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return
+def main():
+    return render_template("main.html")
+
+@app.route('/estilos')
+def estilos():
+    return render_template("estilos.html")
+
+@app.route('/conhecer')
+def conhecer():
+    return render_template("conhecer.html")
+
+@app.route('/prof')
+def prof():
+    return render_template("professores.html")
+
+@app.route('/video')
+def video():
+    return render_template("video.html")
 
 
 if __name__ == '__main__':
